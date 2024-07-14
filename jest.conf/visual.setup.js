f5896a6f629e74ecb9ecc1007082afcb48a9b4e9
Kshitij Thareja <kshitijthareja03@gmail.com>
2024-07-15 00:51:09 +0530

    Test initial workflow

--- jest.conf/visual.setup.js
+++ jest.conf/visual.setup.js
@@ -5,3 +5,19 @@ import { percySnapshot } from '@percy/puppeteer';
 process.env.TEST_TYPE = 'visual';
 
 global.percySnapshot = percySnapshot;
+
+const WAIT_FOR_SELECTOR = '#testing-playground';
+
+const checkPageLoad = async (url, timeout = 30000) => {
+  try {
+    await page.goto(url, { waitUntil: 'networkidle2', timeout });
+    await page.waitForSelector(WAIT_FOR_SELECTOR, { timeout });
+    console.log('Visual testing playground is loaded.');
+  } catch (error) {
+    throw new Error('Failed to load visual testing playground.');
+  }
+};
+
+beforeAll(async () => {
+  await checkPageLoad('http://localhost:4000/testing-playground', 30000);
+});
