bd0f682d907e46d36b8519a02ba8a43e951c7e19
Kshitij Thareja <kshitijthareja03@gmail.com>
2024-07-16 09:46:01 +0530

    Change workflow

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,29 +1,7 @@
-const ci = Boolean(process.env.CI || false);
-
-const baseOptions = {
+module.exports = {
   launch: {
     headless: true,
     timeout: 180000,
   },
   browserContext: 'default',
-}
-
-const ciPipelineOptions = {
-  launch: {
-    executablePath: '/usr/bin/google-chrome-stable',
-    headless: true,
-    args: [
-      '--ignore-certificate-errors',
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-accelerated-2d-canvas',
-      '--disable-gpu'
-    ]
-  },
-  server: {
-    command: 'yarn dev-only',
-    port: 4000
-  }
-}
-
-module.exports = ci ? ciPipelineOptions : baseOptions;
+};
